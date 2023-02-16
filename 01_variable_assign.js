9f39e4bc0b4b633af23b28da0097d816524dd14b
ajaykhabale123 <ajaykhabale123@gmail.com>
2023-02-16 16:10:28 +0530

    Updated the 01 variable assignment
    Updated the 01 variable assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+console.log("******** Step-1 ********");
+console.log("....Variable Declaration....");
+var myFullName
+console.log("My Full Name is:" ,myFullName);
+console.log("....Variable Initialization....");
+var myFullName="Ajay Khabale";
+console.log("My Full Name is:", myFullName );
+console.log("....Variable Value Updated....");
+var myFullName="Ajay Jalindar Khabale";
+console.log("My Full Name is:" ,myFullName);
+console.log("******** Step-2 ********");
+console.log("....Single Variable Declaration and Initialization In same line....");
+var myLove="I love only JS"
+console.log("My Love:", myLove);
+console.log("******** Step-3 ********");
+console.log("....Multiple Variable Declaration and Initialization In same line....");
+var firstName="Ajay";
+var lastName="Khabale";
+var age="24";
+var address="Sangola";
+var pincode="413306";
+var gender="Male";
+var isMarried="Unmarried"
+console.log("My First Name:", firstName, "My Last Name:",lastName,"My Age:",age,"Address:", address,"Pincode:",pincode,"Gender:",gender,"Married or Unmarried",isMarried);
+
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,24 @@
-console.log("Mohit Sharma");
+
+console.log("Ajay Khabale");
+console.log("24");
+console.log("Sangola");
+
+var fullName = "Ajay Khabale";
+console.log(fullName);
+console.log(fullName);
+console.log(fullName);
+
+var city; //Variable Declaraton 
+console.log(city); 
+city = "Sangola"; //variable initialization
+console.log(city); 
+city="Pune"; //variable update
+console.log(city); 
+
+var age="31"; // variable declaration and initialization in same line 
+age="32";
+age="33"; // Variable update 
+console.log(age);
+
+var university="MPKV";
+console.log(university);
