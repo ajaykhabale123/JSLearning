88eaf7e5d79b42da8df9de11e13237a14d3e8316
ajaykhabale123 <ajaykhabale123@gmail.com>
2023-02-17 22:28:07 +0530

    Completed the Assignment 01,02,03
    Completed the assignment 01,02,03 in this assign declare the variables and initialize, variables data type, swap

---
+++ 02_datatypes.js
@@ -0,0 +1,39 @@
+var myName="Virat Kohli";
+var fatherName="Yogesh Kohli"
+
+var age=31;
+var weight= 82.5;
+
+var isMarried=true;
+var islazy= false;
+
+var typeOfMyname= typeof myName;
+console.log("Type of Variable myName:", typeOfMyname);
+console.log("Typeof variable fatherName:", typeof fatherName);
+
+console.log("Type of Variable age:", typeof age);
+console.log("Typeof variable weight:", typeof weight);
+
+console.log("Typeof variable isMarried:", typeof isMarried);
+console.log("Typeof variable isLAzy:", typeof islazy);
+
+var graduation;
+console.log("Value of graduation variable is:", graduation);
+console.log("Typeof variable graduation:", typeof graduation);
+
+var city= null;
+console.log("Value of city variable is:", city);
+console.log("Typeof variable city:", typeof city);
+
+age="32"
+console.log("Typeof variable age:", typeof age);
+
+console.log("Swapping Variables");
+var num1= 100; //200
+var num2= 200; //100
+
+temp=num1;
+num1= num2;
+num2=temp;
+console.log(num1,num2);
+
---
+++ 02_swapVariables.js
@@ -0,0 +1,18 @@
+console.log("********Step-1********");
+var sweety= "Sweety";
+var cutie="Cutie";
+console.log("a-","Sweety:", sweety,"Cutie:", cutie);
+var temp="Sweety";
+sweety=cutie
+cutie=temp
+console.log("b-","Sweety:", sweety,"Cutie:", cutie);
+console.log("********Step-2********");
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
+var temp= 100;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
