3986f889f320e61741f2c5441eb4f886cf3ce96b
ajaykhabale123 <ajaykhabale123@gmail.com>
2023-02-22 11:26:46 +0530

    updated operator and string template files
    updated operator and string template files

---
+++ 04_stringTemplate.js
@@ -0,0 +1,19 @@
+
+
+
+
+console.log(`You are "champ" man `);
+
+var firstName="Mohit";
+var lastName="Sharma";
+var city="Pune";
+
+console.log("First name: ", firstName, "Last Name:",lastName,"City:",city);
+
+console.log(`My first name ${firstName} Last Name: ${lastName} City:${city}`);
+
+var firstName= "Ajay";
+var lastName="Khabale";
+console.log(`My first name ${firstName} Last Name: ${lastName} City:${city}`);
+
+
---
+++ 05_operator.js
@@ -0,0 +1,44 @@
+console.log(`=====Arithmetic Operators====`);
+var num1=10;
+var num2=2;
+var result= num1+num2;// Addition
+console.log(`Addition is ${result}`);
+
+var result=num1-num2;// Subtraction
+console.log(`Subtraction is ${result}`);
+
+var result=num1*num2; //Multiplication
+console.log(`Multiplication is ${result}`);
+
+var result=num1**num2; //Multiplication
+console.log(`Exponential is ${result}`);
+
+var result=num1/3; //Division
+console.log(`Division is ${result}`);
+
+var result=num1 % 3; //Modulus
+console.log(`Reminder is ${result}`);
+
+var num=10; //Compound Addition
+num+=20;
+console.log(`Compound Addition += ${num}`);
+
+console.log(`Comparison Operator `);
+var num3="10";
+var num4=10;
+
+console.log(`Equal Compare==${num3==num4}`);
+console.log(`Strict equal Compare=== ${num3===num4}`);
+
+console.log(`Is not equal to!= ${num3 != num4}`  ); //! not
+
+console.log(`greater Than > ${10>20}`); // > greater than
+
+console.log(`Less than < ${10<20}`); // <less than
+
+var marks = 35; // >= Greater than equal 
+console.log(`gretaer than equal >= ${marks >= 35}`);
+
+
+
+
