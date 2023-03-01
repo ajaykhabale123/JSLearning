e69d2aa80da348c520f481a5d9dde0b5ef969d54
ajaykhabale123 <ajaykhabale123@gmail.com>
2023-03-01 09:17:57 +0530

    Created incrementDecrement and for loop Files
    Created incrementDecrement and for loop Files

---
+++ 05_incrementDecrementOpr.js
@@ -0,0 +1,28 @@
+
+
+
+// index++ ==> index = index+1
+//index- ==> index = index -1
+var index =10;
+var y = index +1;
+console.log(`index: ${index}, y: ${y}`);
+
+console.log("Post Increment");
+var index =10;
+var y = index ++;
+console.log(`index: ${index}, y: ${y}`);
+
+console.log("Pre Increment");
+var index =10;
+var y = ++index ;
+console.log(`index: ${index}, y: ${y}`);
+
+console.log("Post Decrement");
+var index =10;
+var y = index --;
+console.log(`index: ${index}, y: ${y}`);
+
+console.log("Pre Decrement");
+var index =10;
+var y = --index ;
+console.log(`index: ${index}, y: ${y}`);
---
+++ 06_for_loop.js
@@ -0,0 +1,29 @@
+
+
+//print numbers from 1-10
+//initialization 1 
+//update Expression +1
+//condition <=10
+for (let index = 1; index<=10; index= index+1) {
+   console.log(index);  
+}
+console.log("1 to 5 ");
+for (let index = 0; index <=5;index= index+1) {
+
+    console.log(index);   
+}
+console.log("Reverse Condition");
+for (let index = 10; index>=0 ;index= index-1){
+   console.log(index);
+
+}
+console.log("Table of 5");
+for (let index = 5; index <=50; index= index+5) {
+    console.log(index);
+    
+}
+console.log("******************************************");
+// for (let index = 1; index < 10;){
+//     console.log(index);   
+// }
+
