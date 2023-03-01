269e1c9bbe6882356a447ba717978fac3fc15e69
ajaykhabale123 <ajaykhabale123@gmail.com>
2023-03-01 10:10:25 +0530

    Completed Assignment 06_for_Assignment
    Completed Assignment 06_for_Assignment

---
+++ 06_for_Assignment.js
@@ -0,0 +1,34 @@
+console.log('Step 1 : WAP to print numbers from 5 to 15 by incrementing')
+for (let index = 5; index <= 15; index = index + 1) {
+  console.log(index)
+}
+
+console.log('Step 2 : WAP to print numbers from 50 to 40 by decrementing by 1')
+for (let index = 50; index >= 40; index = index - 1) {
+  console.log(index)
+}
+
+console.log('Step 3 : WAP to find first 15 odd numbers')
+for (let index = 1; index <= 30; index = index + 2) {
+  console.log(index)
+}
+
+console.log('Step 4 : WAP to find first 10 even numbers')
+for (let index = 0; index < 20; index = index + 2) {
+  console.log(index)
+}
+
+console.log('Step 5 : WAP to print table of 5 ')
+for (let index = 5; index <= 50; index = index + 5) {
+  console.log(index)
+}
+
+console.log('Step 6 : WAP to print table of 10 ')
+for (let index = 10; index <= 100; index = index + 10) {
+  console.log(index)
+}
+
+console.log('Step 7 : WAP to print table of 10 in reverse order ')
+for (let index = 100; index >= 10; index = index - 10) {
+  console.log(index)
+}
